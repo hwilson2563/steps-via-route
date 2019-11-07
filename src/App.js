3677ad9ffde02bea0a3beffcc56ddd95afed0d1a
Hilary Matusiak <hilary.matusiak@woodlanddirect.com>
2019-11-07 12:30:53 -0500

    create route for form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Header from './components/layout/Header'
 import Sidebar from './components/layout/Sidebar'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
-
+import NameForm from './components/prompt/NameForm'
 const App = () => {
   return (
     <Router>
       <Header />
       <Sidebar />
       <div className='rightContentContainer'>
-        <Route path='/' component={Home} />
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/prompt' component={NameForm} exact />
+        </Switch>
       </div>
     </Router>
   )
